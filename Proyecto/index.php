ee2e74954cfff5cd47fc8a245c5ab0d289021e16
JavierMarttinez <Javierzarts@gmail.com>
2016-07-14 12:36:19 -0500

    Carsoft
    Parceros

---
+++ Proyecto/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Documento sin t&iacute;tulo</title>
+</head>
+
+<body>
+<table width="875" height="648" border="1" align="center">
+  <tr>
+    <td height="209" colspan="2"><img src="../../../Users/ADSI/Pictures/come cocos cenando.jpg" width="864" height="205" /></td>
+  </tr>
+  <tr>
+    <td width="183" height="187">&nbsp;</td>
+    <td width="676"><form id="form2" name="form2" method="post" action="">
+      <label>Escribe Tu Nombre:
+        <input type="text" name="textfield2" />
+        </label>
+      <p>
+        <label>Escribe Tu Documento
+        <input type="text" name="textfield3" />
+        <br />
+        <br />
+        </label>
+        <input type="submit" name="Submit" value="Enviar" />
+      </p>
+    </form>
+      </td>
+  </tr>
+  <tr>
+    <td height="242" colspan="2">&nbsp;</td>
+  </tr>
+</table>
+<form id="form1" name="form1" method="post" action="">
+  <label>
+  <input type="text" name="textfield" />
+  </label>
+</form>
+</body>
+</html>
