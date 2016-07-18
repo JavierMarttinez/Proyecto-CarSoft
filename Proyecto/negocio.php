4de31d4e01aea8094f2719cf75fb0d5cde3fb160
JavierMarttinez <Javierzarts@gmail.com>
2016-07-18 13:44:40 -0500

    New Act
    123456

--- Proyecto/negocio.php
+++ Proyecto/negocio.php
@@ -11,61 +11,84 @@
 </head>
 
 <body>
-<table width="1146" height="757" border="0" align="center" bgcolor="#CCCCCC">
+<table width="1130" height="771" border="0" align="center" bgcolor="#CCCCCC">
   <tr>
-    <td height="230" colspan="2"><img src="Imagenes/9874651.png" width="305" height="203" align="absbottom" /><img src="Imagenes/carros de lujo (4).jpg" width="264" height="228" /><img src="Imagenes/fotos-de-coches-deportivos-de-lujo-tuneados1.jpg" width="264" height="226" /><img src="Imagenes/97811_coches-deportivos-3.jpg" width="266" height="227" /></td>
+    <td height="230" colspan="2"><img src="Imagenes/54654.png" width="1281" height="307" /></td>
   </tr>
   <tr>
-    <td width="1040" height="24" bgcolor="#000000"><object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
+    <td width="1174" height="224" bgcolor="#CCCCCC"><form id="form2" name="form2" method="post" action="negocio.php">
+      <label></label>
+      <p align="center" class="Estilo1">Ingresar</p>
+      <p align="center" class="Estilo1">&nbsp;</p>
+      <p>
+      <label>
+      <div align="center">
+      <div align="center">Escribe Tu Nombre De Usuario:
+        <input name="Nombre" type="text" id="Nombre" />
+      </div>
+      </label>
+      <p align="center">
+        <label>Escribe Tu Contrase&ntilde;a
+          <input name="Docuemento" type="password" id="Docuemento" />
+          <br />
+          <br />
+          </label>
+        <label for="Submit"></label>
+        <label for="Submit"></label>
+        <input type="submit" name="Submit" value="Entrar" id="Submit" />
+        <label></label>
+      </p>
+      <p>
+      <label>
+      <div align="center">
+      <div align="center"><a href="Demalas.php"> &iquest;Olvidaste Tu Contrase&ntilde;a? </a><br />
+          <a href="#">&iquest;Deseas Crear Cuenta?</a></div>
+      </label>
+      <label></label>
+    </form></td>
+    <td width="103" bgcolor="#CCCCCC"><object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
       <param name="BGCOLOR" value="" />
-      <param name="movie" value="button3.swf" />
+      <param name="movie" value="button9.swf" />
       <param name="quality" value="high" />
-      <embed src="button3.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
+      <embed src="button9.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
     </object>
-      <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
-        <param name="movie" value="button4.swf" />
-        <param name="quality" value="high" />
-        <embed src="button4.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
-      </object>
-      <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
-        <param name="movie" value="button5.swf" />
-        <param name="quality" value="high" />
-        <embed src="button5.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
-      </object>
-      <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
-        <param name="movie" value="button6.swf" />
-        <param name="quality" value="high" />
-        <embed src="button6.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
-      </object>
-      <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
-        <param name="BGCOLOR" value="" />
-        <param name="movie" value="button7.swf" />
-        <param name="quality" value="high" />
-        <embed src="button7.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
-      </object>
-      <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
-        <param name="movie" value="button8.swf" />
-        <param name="quality" value="high" />
-        <embed src="button8.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
-      </object>
-      <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
-        <param name="BGCOLOR" value="" />
-        <param name="movie" value="button9.swf" />
-        <param name="quality" value="high" />
-        <embed src="button9.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
-    </object>
-      <img src="Imagenes/Twitter-icon.png" width="38" height="36" /></td>
-    <td width="181" height="24">&nbsp;
-	
-	
-	</td>
+        <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
+          <param name="BGCOLOR" value="" />
+          <param name="movie" value="button7.swf" />
+          <param name="quality" value="high" />
+          <embed src="button7.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
+        </object>
+        <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
+          <param name="BGCOLOR" value="" />
+          <param name="movie" value="button3.swf" />
+          <param name="quality" value="high" />
+          <embed src="button3.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
+        </object>
+        <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
+          <param name="movie" value="button4.swf" />
+          <param name="quality" value="high" />
+          <embed src="button4.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
+        </object>
+        <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
+          <param name="movie" value="button8.swf" />
+          <param name="quality" value="high" />
+          <embed src="button8.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
+        </object>
+        <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
+          <param name="movie" value="button6.swf" />
+          <param name="quality" value="high" />
+          <embed src="button6.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
+        </object>
+        <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
+          <param name="movie" value="button5.swf" />
+          <param name="quality" value="high" />
+          <embed src="button5.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
+        </object>
+    </td>
   </tr>
   <tr>
-    <td colspan="2">&nbsp;</td>
-  </tr>
-  
-  <tr>
-    <td height="230" colspan="2"><label></label><p><a href="negocio.php">Volver Al Inicio</a><img src="Imagenes/94656.png" width="1192" height="331" /></p></td>
+    <td height="230" colspan="2" background="Imagenes/94656.png"><label></label>
+        <p align="left">&nbsp;</p></td>
   </tr>
 </table>
 </body>
